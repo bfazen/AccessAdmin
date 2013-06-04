35669df17358a90338a3202de9f42fc527897cb7
Louis Fazen <louis.fazen@gmail.com>
2013-06-04 18:23:16 +0300

    Adding New MessageHold Activity
    Fixing MessageHold Activity, adding new feature of AlarmHold for
    locking devices with a clear message and sound alarm after, for
    example, the SIM card has been changed.  Intended to increase
    compliance with not removing SIM card in Kosirai.

--- src/com/alphabetbloc/accessadmin/activities/MessageHoldActivity.java
+++ src/com/alphabetbloc/accessadmin/activities/MessageHoldActivity.java
@@ -20,6 +20,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
@@ -29,8 +31,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.alphabetbloc.accessadmin.R;
@@ -40,58 +44,167 @@ import com.alphabetbloc.accessadmin.receivers.AirplaneOffReceiver;
 
 public class MessageHoldActivity extends Activity implements OnTouchListener {
 
-	protected static final int DEVICE_ADMIN = 1;
-	public static final String STOP_HOLD = "stop_hold";
-	private String mToast = "";
-	public static boolean sMessageHoldActive = false;
 	private static final String TAG = MessageHoldActivity.class.getSimpleName();
+
+	// View type
+	public static final String STOP_HOLD = "stop_hold";
+	public static final String MESSAGE = "message";
+	public static final String SUBMESSAGE = "submessage";
+	public static final String ADDITIONAL_INFO = "additional_info";
+	private int mHoldType = 0;
+	private String mMessage = "";
+	private String mSubMessage = "";
+	private String mAdditionalInfo = "";
+	private boolean mPermanentHold = false;
+
+	// Alarm
+	private boolean mSoundAlarm = false;
+	private MediaPlayer mPlayer;
+	private boolean mVolumeUp = false;
+
+	// Network and life cycle
+	public static boolean sMessageHoldActive = false;
 	private boolean mNetworkOnMode = false;
 	private AirplaneOffReceiver mNetworkOnReceiver;
 
-	
+	// Options Menu
+	private static final int DEVICE_ADMIN = 1;
+	private static final int ADMIN_OPTIONS = 0;
+	private static final int RETURN_PERM_HOLD = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.message_hold);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		// start, stop, or change the message of the activity
 		boolean stopHold = getIntent().getBooleanExtra(STOP_HOLD, false);
-		if(stopHold){
-			if (mNetworkOnMode) {
-				unregisterReceiver(mNetworkOnReceiver);
-				mNetworkOnMode = false;
-			}
+		if (stopHold) {
+			unregisterAirplaneOffReceiver();
+			mPermanentHold = false;
 			finish();
 		} else {
-			mNetworkOnMode = true;
+			createView();
 		}
 	}
-	
+
+	private void createView() {
+		// create the correct view
+		mHoldType = getIntent().getIntExtra(Constants.HOLD_TYPE, 0);
+		switch (mHoldType) {
+		case Constants.SIM_ERROR:
+		case Constants.DEVICE_LOCKED:
+			mPermanentHold = true;
+			mSoundAlarm = true;
+			setContentView(R.layout.hold_alarm);
+			break;
+		case Constants.ADMIN_MESSAGE:
+		default:
+			mPermanentHold = false;
+			mSoundAlarm = false;
+			setContentView(R.layout.admin_message);
+			break;
+		}
+
+		// Set the text content
+		// Message
+		mMessage = getIntent().getStringExtra(MESSAGE);
+		TextView message = (TextView) findViewById(R.id.message);
+		if (mMessage != null)
+			message.setText(mMessage);
+
+		// Sub-Message
+		mSubMessage = getIntent().getStringExtra(SUBMESSAGE);
+		TextView submessage = (TextView) findViewById(R.id.submessage);
+		if (mSubMessage != null)
+			submessage.setText(mSubMessage);
+
+		// Additional Info
+		mAdditionalInfo = getIntent().getStringExtra(ADDITIONAL_INFO);
+		RelativeLayout additionalInfo = (RelativeLayout) findViewById(R.id.additional_info);
+		TextView additionalMessage = (TextView) findViewById(R.id.additional_message);
+		if (mAdditionalInfo != null) {
+			additionalInfo.setVisibility(View.VISIBLE);
+			additionalMessage.setText(mMessage);
+		} else {
+			additionalInfo.setVisibility(View.GONE);
+		}
+	}
+
+	private void startAlarm() {
+		if (mPlayer == null) {
+			setVolumeControlStream(AudioManager.STREAM_MUSIC);
+			AudioManager aM = (AudioManager) getSystemService(AUDIO_SERVICE);
+			aM.setStreamVolume(AudioManager.STREAM_MUSIC, aM.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+			mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
+			mPlayer.setLooping(true);
+		}
+		if (mPlayer.isPlaying())
+			return;
+		else
+			mPlayer.start();
+	}
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		if (mNetworkOnMode)
-			registerAirplaneOffReceiver();
 		sMessageHoldActive = true;
-		mToast = getIntent().getStringExtra(Constants.TOAST_MESSAGE);
-		TextView tv = (TextView) findViewById(R.id.message);
-		tv.setText(mToast);
+		registerAirplaneOffReceiver();
+		// if (mSoundAlarm)
+		// startAlarm();
+		Log.v(TAG, "OnResume Called");
 	}
 
-	
 	@Override
 	protected void onPause() {
 		super.onPause();
 		sMessageHoldActive = false;
-		if (mNetworkOnMode){
-			unregisterReceiver(mNetworkOnReceiver);
-			Log.v(TAG, "unregistering airplane receiver");
+		unregisterAirplaneOffReceiver();
+		Log.v(TAG, "OnPause Called");
+		if (mPermanentHold) {
+			Intent i = new Intent(getApplicationContext(), MessageHoldActivity.class);
+			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			i.putExtra(Constants.HOLD_TYPE, mHoldType);
+			i.putExtra(MessageHoldActivity.MESSAGE, mMessage);
+			i.putExtra(MessageHoldActivity.SUBMESSAGE, mSubMessage);
+			i.putExtra(MessageHoldActivity.ADDITIONAL_INFO, mAdditionalInfo);
+			getApplicationContext().startActivity(i);
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onDestroy();
+		stopAlarm();
+	}
+
+	private void stopAlarm() {
+		if (mPlayer != null) {
+			if (mPlayer.isPlaying()) {
+				try {
+					mPlayer.stop();
+				} catch (Exception e) {
+					Log.e(TAG, "MediaPlayer already stopped.");
+				}
+			}
+
+			mPlayer.release();
+			mPlayer = null;
 		}
 	}
 
 	private void registerAirplaneOffReceiver() {
+		if (mNetworkOnMode)
+			return;
+
+		mNetworkOnMode = true;
 		mNetworkOnReceiver = new AirplaneOffReceiver();
 		IntentFilter airplaneFilter = new IntentFilter(Constants.AIRPLANE_MODE);
 		registerReceiver(mNetworkOnReceiver, airplaneFilter);
-		if(Constants.DEBUG) Log.v(TAG, "registering airplane receiver");
+		if (Constants.DEBUG)
+			Log.v(TAG, "registering airplane receiver");
+
 		boolean enabled = Settings.System.getInt(getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0) == 1;
 		if (enabled) {
 			Settings.System.putInt(getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0);
@@ -99,108 +212,140 @@ public class MessageHoldActivity extends Activity implements OnTouchListener {
 			i.putExtra("state", 0);
 			sendBroadcast(i);
 		}
+
 	}
-	
-	
-	// ////// CONSUMES ALL UI EVENTS ////////
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			super.onCreateOptionsMenu(menu);
-			menu.add(0, DEVICE_ADMIN, 0, getString(R.string.device_admin)).setIcon(android.R.drawable.ic_menu_preferences);
-			return true;
-		}
 
-		@Override
-		public boolean onPrepareOptionsMenu(Menu menu) {
-			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-			boolean showMenu = prefs.getBoolean(Constants.SHOW_MENU, false);
-			Policy policy = new Policy(this);
-			if (!policy.isAdminActive() || !showMenu)
-				return false;
-			return super.onPrepareOptionsMenu(menu);
+	private void unregisterAirplaneOffReceiver() {
+		if (mNetworkOnMode) {
+			mNetworkOnMode = false;
+			unregisterReceiver(mNetworkOnReceiver);
+			Log.v(TAG, "unregistering airplane receiver");
 		}
+	}
 
-		@Override
-		public boolean onOptionsItemSelected(MenuItem item) {
-			switch (item.getItemId()) {
-			case DEVICE_ADMIN:
-				Intent ip = new Intent(getApplicationContext(), AdminLoginActivity.class);
-				startActivity(ip);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-			}
-		}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, DEVICE_ADMIN, 0, getString(R.string.device_admin)).setIcon(android.R.drawable.ic_menu_preferences);
+		return true;
+	}
 
-		// Consume all UI events except Menu, Exit and Pwd Btns
-		@Override
-		public void onAttachedToWindow() {
-			super.onAttachedToWindow();
-			this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
-		}
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		boolean showMenu = prefs.getBoolean(Constants.SHOW_MENU, false);
+		Policy policy = new Policy(this);
 
-		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-			// NB: Override this in all child activities
+		if (!policy.isAdminActive() || !showMenu || !mVolumeUp)
 			return false;
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case DEVICE_ADMIN:
+			Intent i = new Intent(getApplicationContext(), AdminLoginActivity.class);
+			if (mPermanentHold) {
+				mPermanentHold = false;
+				startActivityForResult(i, RETURN_PERM_HOLD);
+			} else
+				startActivityForResult(i, ADMIN_OPTIONS);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == RETURN_PERM_HOLD)
+			mPermanentHold = true;
+	}
+
+	// Consume all UI events except Menu, Exit and Pwd Btns
+	@Override
+	public void onAttachedToWindow() {
+		super.onAttachedToWindow();
+		this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// NB: Override this in all child activities
+		return false;
+	}
+
+	@Override
+	public void onBackPressed() {
+		return;
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			event.startTracking();
+			return true;
 		}
 
-		@Override
-		public void onBackPressed() {
-			return;
+		if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_SEARCH || keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+			return true;
 		}
 
-		@Override
-		public boolean onKeyDown(int keyCode, KeyEvent event) {
-			if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_SEARCH || keyCode == KeyEvent.KEYCODE_BACK) {
+		if (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+			mVolumeUp = true;
+		return false;
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			openOptionsMenu();
+			return true;
+		}
+
+		if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_SEARCH || keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+			return true;
+		}
+		if (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+			mVolumeUp = false;
+		return false;
+	}
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+		if (event.getAction() == KeyEvent.ACTION_DOWN || event.getAction() == KeyEvent.ACTION_UP) {
+			if (event.getKeyCode() == KeyEvent.KEYCODE_HOME || event.getKeyCode() == KeyEvent.KEYCODE_SEARCH || event.getKeyCode() == KeyEvent.KEYCODE_BACK || event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
 				return true;
 			}
-			return false;
 		}
+		return super.dispatchKeyEvent(event);
+	}
 
-		@Override
-		public boolean onKeyUp(int keyCode, KeyEvent event) {
-			if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_SEARCH || keyCode == KeyEvent.KEYCODE_BACK) {
-				return true;
-			}
-			return false;
-		}
+	@Override
+	public boolean dispatchTrackballEvent(MotionEvent ev) {
+		return true;
+	}
 
-		@Override
-		public boolean dispatchKeyEvent(KeyEvent event) {
-			if (event.getAction() == KeyEvent.ACTION_DOWN || event.getAction() == KeyEvent.ACTION_UP) {
-				if (event.getKeyCode() == KeyEvent.KEYCODE_HOME || event.getKeyCode() == KeyEvent.KEYCODE_SEARCH || event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-					return true;
-				}
-			}
-			return super.dispatchKeyEvent(event);
-		}
+	@Override
+	public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+		return true;
+	}
 
-		@Override
-		public boolean dispatchTrackballEvent(MotionEvent ev) {
-			return true;
-		}
+	@Override
+	public boolean onKeyMultiple(int keyCode, int repeatCount, KeyEvent event) {
+		return true;
+	}
 
-		@Override
-		public boolean onKeyLongPress(int keyCode, KeyEvent event) {
-			return true;
-		}
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return true;
+	}
 
-		@Override
-		public boolean onKeyMultiple(int keyCode, int repeatCount, KeyEvent event) {
-			return true;
-		}
-
-		@Override
-		public boolean onTouchEvent(MotionEvent event) {
-			return true;
-		}
-
-		@Override
-		public boolean onTrackballEvent(MotionEvent event) {
-			return true;
-		}
+	@Override
+	public boolean onTrackballEvent(MotionEvent event) {
+		return true;
+	}
 
 }
-
-
