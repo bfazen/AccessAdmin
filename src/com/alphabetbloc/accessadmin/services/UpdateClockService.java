86844f92537629487ef45ca2238efff0d474d0d7
Louis Fazen <louis.fazen@gmail.com>
2014-01-31 08:08:51 -0500

    apache license
    added the apache license, version 2 to all source files.

--- src/com/alphabetbloc/accessadmin/services/UpdateClockService.java
+++ src/com/alphabetbloc/accessadmin/services/UpdateClockService.java
@@ -1,7 +1,5 @@
-package com.alphabetbloc.accessadmin.services;
-
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2008 The Android Open Source Project, Copyright (C) 2012 Louis Fazen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +13,9 @@ package com.alphabetbloc.accessadmin.services;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.alphabetbloc.accessadmin.services;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
